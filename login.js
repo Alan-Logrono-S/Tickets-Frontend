dc0295f82b329b398f73f6718b1b12cb022bd98c
Alan-Logroño-S <alanjologrono@hotmail.com>
2026-02-25 10:16:55 -0500

    Logica Login JS inicio de Sesion

---
+++ login.js
@@ -0,0 +1,27 @@
+const errorDiv =  document.querySelector('#mensajeError');
+
+document.querySelector('#form-login').addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    try{
+        res = await fetch('http://localhost:4000/api/login/usuarios',{
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({
+                email: document.querySelector('#email').value,
+                password: document.querySelector('#password').value
+            })
+        });
+        const data = await res.json();
+        if(!res.ok) throw new Error(data.msg || "Error en el Login");
+        localStorage.setItem('usuario', JSON.stringify(data));
+        window.location.href = 'dashboard.html'
+    }catch(error){
+        errorDiv.textContent = error.message;
+        errorDiv.classList.remove('errorHiden');
+        errorDiv.classList.add('errorShow');
+        setTimeout(() => {
+            errorDiv.classList.remove('errorShow');
+            errorDiv.classList.add('errorHiden');
+        }, 3000);
+    };
+});
